f601cc825cdf07b1bfd6b10fe6aa8c23aa9c94e2
huangliyuan <443687124@QQ.COM>
2018-03-26 10:40:12 +0800

    首次上传

---
+++ Assets/_Scripts/P4TVScreenPlay.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class P4TVScreenPlay : MonoBehaviour
+{
+    [SerializeField]
+    MovieTexture m_movie;
+
+    [SerializeField]
+    Texture m_trouble;
+
+    enum P4TVState
+    {
+        normalState,
+        abnormalState,
+    }
+	void Start ()
+    {
+        int p4TVStatus = Random.Range(0, 2);
+        TVStatus((P4TVState)p4TVStatus);
+    }
+	
+    void TVStatus(P4TVState StatusNum)
+    {
+        switch (StatusNum)
+        {
+            case P4TVState.normalState:
+                {
+                    GetComponent<Renderer>().material.mainTexture = m_movie;
+                    m_movie.loop = true;
+                    m_movie.Play();
+                    break;
+                }
+            case P4TVState.abnormalState:
+                {
+                    GetComponent<Renderer>().material.mainTexture = m_trouble;
+                    break;
+                }
+            default:
+                break;
+        }
+    }
+	
+}
---
+++ Assets/_Scripts/TestWall.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestWall : MonoBehaviour
+{
+    [SerializeField]
+    GameObject m_glassWallL;
+    [SerializeField]
+    GameObject m_glassWallR;
+
+
+    float xPos;
+    float yPos;
+    float zPos;
+	void Start ()
+    {
+        NewPosition(m_glassWallL);
+        NewPosition(m_glassWallR);
+    }
+
+    void NewPosition(GameObject tempObject)
+    {
+        xPos = tempObject.transform.localPosition.x;
+        yPos = tempObject.transform.localPosition.y;
+        zPos = tempObject.transform.localPosition.z;
+        for (int i = 0; i < 20; i++)
+        {
+            Instantiate(tempObject, new Vector3(xPos, yPos, zPos), Quaternion.Euler(0, 90, 0), transform.parent);
+            xPos -= 4.1f;
+        }
+    }
+
+	void Update ()
+    {
+		
+	}
+}
